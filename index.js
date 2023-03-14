e7ecd4e0d1c440b2a51dce24aca5ad79fc2ca4f7
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2023-03-14 17:09:13 -0300

    Feat: Scripts, rotas de usuário e controller

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectToDatabase = require('./src/database/database');
+const usuario = require('./src/router/usuario.router');
 
 const app = express();
 
@@ -8,6 +9,8 @@ const port = 3000;
 app.use(express.json());
 
 connectToDatabase();
+
+app.use('/usuario', usuario);
 
 app.get('/', (req, res) => {
     res.send({
