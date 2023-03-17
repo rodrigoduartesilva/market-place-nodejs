2e380efb62911b114b7b450bbd931d5868d43bb0
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2023-03-17 17:43:39 -0300

    Feat: Add - RM categoria no produto.

--- src/controller/produto.controller.js
+++ src/controller/produto.controller.js
@@ -51,10 +51,31 @@ const deleteProductController = async (req, res) => {
     }
 }
 
+const addCategoriaProdutoController = async (req, res) => {
+    try {
+        req.body.createdAt = new Date();
+        const categoria = await produtoService.addCategoriaProdutoService(req.params.id, req.body);
+    } catch {
+        console.log(`erro: ${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado, tente novamente.` });
+    }
+}
+
+const removeCategoriaProdutoController = async (req, res) => {
+    try {
+        const categoria = await produtoService.removeCategoriaProdutoService(req.body);
+    } catch {
+        console.log(`erro: ${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado, tente novamente.` });
+    }
+}
+
 module.exports = {
     findProductByIdController,
     findAllProductsController,
     createProductController,
     updateProductController,
-    deleteProductController
+    deleteProductController,
+    addCategoriaProdutoController,
+    removeCategoriaProdutoController
 }
