be236636ffce4e5271f5a6006bc051db1a80f114
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2023-03-18 17:15:15 -0300

    Feat: Valida login implementado.

--- src/middleware/validacao.middleware.js
+++ src/middleware/validacao.middleware.js
@@ -125,11 +125,35 @@ const validaId = (req, res, next) => {
     }
 }
 
+const validaLogin = (req, res, next) => {
+    let erros = []; //array para acumular os erros.
+
+    if (!req.body.email) {
+        erros.push('email');
+    }
+
+    if (!req.body.senha) {
+        erros.push('senha');
+    }
+
+    //teste de quantos erros ocorreram e as devidas ações a serem realizadas.
+    if (erros.length == 0) {
+        return next();
+    } else {
+        if (erros.length > 1) {
+            return res.status(400).send({ messsage: `Os campos ${erros} precisam ser preenchidos.` });
+        } else {
+            return res.status(400).send({ messsage: `O campo ${erros} precisa ser preenchido.` });
+        }
+    }
+}
+
 module.exports = {
     validaUsuario,
     validaProduto,
     validaCategoria,
     validaPedido,
     validaCarrinho,
-    validaId
+    validaId,
+    validaLogin
 }
